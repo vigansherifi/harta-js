6ec791d22d37f90e20a3787cdab01fde6729d8e1
vigansherifi <vigansherifi11@gmail.com>
2025-05-05 16:51:53 +0200

    Add files via upload

---
+++ harta/script.js
@@ -0,0 +1,42 @@
+var map = L.map('map').setView([42.65523136994448, 21.164293022117498], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var greenIcon = L.icon({
+    iconUrl: 'leaf-green.png',
+    shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map);
+L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map).bindPopup("I am a green leaf.");
+
+
+
+
+
+
+// var marker = L.marker([42.460985078708234, 21.46513964088776]).addTo(map);
+
+// var circle = L.circle([51.508, -0.11], {
+//     color: 'green',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(map);
+
+// var polygon = L.polygon([
+//     [51.509, -0.08],
+//     [51.503, -0.06],
+//     [51.51, -0.047]
+// ]).addTo(map);
+
+// marker.bindPopup('<b>Lokacioni i Jcoders</b> <img src="download.jpg">').openPopup();
